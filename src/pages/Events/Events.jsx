61a9dddff478aec525fbd8f4da154b6fa0cfe8cc
Dipayan Maji <dipayanmaji1112@gmail.com>
2023-03-30 16:56:14 +0530

    upcoming event remaining timmer done

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -9,6 +9,11 @@ import event5 from '../../images/events/event5.jpg';
 import event6 from '../../images/events/event6.jpg';
 import event7 from '../../images/events/event7.jpg';
 import event8 from '../../images/events/event8.jpg';
+import image1 from '../../images/drink-water.png';
+import image2 from '../../images/share-water.jpg';
+import image3 from '../../images/water-boy-smile.jpg';
+
+import { getTimeRemaining } from './timer';
 
 const eventsList = [
     {
@@ -79,15 +84,57 @@ const eventsList = [
 
 const Events= ()=>{
     const [nonactive, setNonactive] = useState(true);
+    const [color, setColor] = useState('');
+    const [remainingTime, setRemainingTime] = useState(getTimeRemaining());
+
     useEffect(()=>{
         setNonactive(false);
+
+        let eventTimerInerval = setInterval(()=>{
+            let random1 = Math.floor(Math.random() * 256)
+            let random2 = Math.floor(Math.random() * 256)
+            let random3 = Math.floor(Math.random() * 256)
+            setColor(`rgb(${random1}, ${random2}, ${random3})`);
+
+            const leftTime = getTimeRemaining()
+            setRemainingTime(leftTime);
+            console.log('event timmer running');
+
+            if(leftTime.days <= 0 && leftTime.hours <= 0 && leftTime.minutes <= 0 && leftTime.seconds <= 0 ){
+                console.log('Event Remaining Time Is Ended.');
+                setColor('');
+                clearInterval(eventTimerInerval);
+            }
+
+        },1000)
+
+        return ()=>{
+            console.log('event timmer paused');
+            clearInterval(eventTimerInerval);
+        }
     },[])
+    
     return(
         <div className={`events-container`}>
             <h1 className={`heading ${nonactive ? 'nonactive-heading' : ''}`}>Our Events</h1>
 
-            <div className='upcoming-event'>
-                <h1>THE HEALTHIER</h1>
+            <div className={`upcoming-event ${nonactive ? 'nonactive-upcoming-events' : ''}`}>
+                <section className='upcoming-heading'>
+                    <h2>HEALTH:WATER</h2>
+                    <span>on 7th April, 2023</span>
+                </section>
+                <p>on the occasion of World Health Day</p>
+                <div className='images'>
+                    <img id='image1' src={image1} alt="Girl Drink Water" />
+                    <img id='image2' src={image2} alt="Sharing Water" />
+                    <img id='image3' src={image3} alt="Boy with Water" />
+                </div>
+                <div className='timer'>
+                    <strong id='days'>DAYS: <span>{remainingTime.days}</span></strong>
+                    <strong id='hours'>HOURS: <span>{remainingTime.hours}</span></strong>
+                    <strong id='minutes'>MINUTES: <span>{remainingTime.minutes}</span></strong>
+                    <strong id='seconds'>SECONDS: <span style={{color: color}}>{remainingTime.seconds}</span></strong>
+                </div>
             </div>
 
             <div className={`events ${nonactive ? 'nonactive-events' : ''}`}>
---
+++ src/pages/Events/timer.js
@@ -0,0 +1,30 @@
+const eventDate = new Date('2023/4/7');
+
+export const getTimeRemaining= ()=>{
+    const total = eventDate - new Date();
+    if(total <= 0){
+        return{
+            days: '00',
+            hours: '00',
+            minutes: '00',
+            seconds: '00',
+        }
+    }
+
+    let seconds = Math.floor( (total/1000) % 60 );
+    let minutes = Math.floor( (total/1000/60) % 60 );
+    let hours = Math.floor( (total/(1000*60*60)) % 24 );
+    let days = Math.floor( total/(1000*60*60*24) );
+
+    if(seconds < 10) seconds = '0'+seconds;
+    if(minutes < 10) minutes = '0'+minutes;
+    if(hours < 10) hours = '0'+hours;
+    if(days < 10) days = '0'+days;
+
+    return {
+        days,
+        hours,
+        minutes,
+        seconds
+    };
+}
