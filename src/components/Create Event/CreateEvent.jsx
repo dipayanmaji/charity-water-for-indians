059e6875707d53b10127ba42b36e59bf51136f52
Dipayan Maji <121128467+dipayanmaji@users.noreply.github.com>
2023-03-31 16:00:54 +0530

    change the a tag with Link tag

--- src/components/Create Event/CreateEvent.jsx
+++ src/components/Create Event/CreateEvent.jsx
@@ -1,15 +1,16 @@
 import './CreateEvent.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const CreateEvent= ({event})=>{
-    return(
+const CreateEvent = ({ event }) => {
+    return (
         <figure className="event">
             <h5>{event.name}, {event.year}</h5>
-            <img src={event.image} alt={event.name}/>
+            <img src={event.image} alt={event.name} />
             <figcaption>
                 <div><h2><span>{event.name}</span> in {event.state}</h2></div>
-                <div><p>In the year of {event.year}, <Wrapper>{event.name}</Wrapper> programme goes through in the <Wrapper>{event.district}</Wrapper> district, <Wrapper>{event.state}</Wrapper>, India. <br /> <a href="#">Read More...</a></p></div>
+                <div><p>In the year of {event.year}, <Wrapper>{event.name}</Wrapper> programme goes through in the <Wrapper>{event.district}</Wrapper> district, <Wrapper>{event.state}</Wrapper>, India. <br /> <Link to={'/our-work'}>Read More...</Link></p></div>
             </figcaption>
         </figure>
     )
