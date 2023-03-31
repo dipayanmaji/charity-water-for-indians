04bd116a8ab676891c6d927bece046a02ae31a9f
Dipayan Maji <121128467+dipayanmaji@users.noreply.github.com>
2023-03-31 16:05:22 +0530

    change into some of flex and links

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,11 @@
 import "./Hero.css"
+import { useNavigate } from "react-router-dom";
 import React from "react";
 
 const Hero = (props) => {
+  const navigate = useNavigate();
   return (
-    <>
+    <div className="hero-container">
       <div className={props.cName}>
         <img alt="mainImg" src={props.HeroImg} />
       </div>
@@ -26,27 +28,27 @@ const Hero = (props) => {
             <div class="col-sm-6 col-md-4">
               <div class="team-item">
                 <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team1a.jpg" class="team-img" alt="pic" />
-                <h3>JOHNATHAN HAWKINS</h3>
+                <h3>NIKHILESH YADAV</h3>
                 <div class="team-info"><p>Head of SEO</p></div>
-                <p>Johnathan is our  co-founder and has developed search strategies for a variety of clients from international brands to medium sized businesses for over five years.</p>
+                <p>Nikhilesh is our  co-founder and has developed search strategies for a variety of clients from international brands to medium sized businesses for over five years.</p>
               </div>
             </div>
 
             <div class="col-sm-6 col-md-4">
               <div class="team-item">
                 <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team2a.jpg" class="team-img" alt="pic" />
-                <h3>ALEXANDRA SMITHS</h3>
+                <h3>DRIKSHA KOYEN</h3>
                 <div class="team-info"><p>SEO Specialist</p></div>
-                <p>Graduating with a degree in Spanish and English, Alexandra has always loved writing and now she’s lucky enough to do it as part of her new job inside our agency.</p>
+                <p>Graduating with a degree in Spanish and English, Driksha has always loved writing and now she’s lucky enough to do it as part of her new job inside our agency.</p>
               </div>
             </div>
 
             <div class="col-sm-6 col-md-4">
               <div class="team-item">
                 <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team3a.jpg" class="team-img" alt="pic" />
-                <h3>CARTER HARRINGTON</h3>
+                <h3>KUMAR JADAV</h3>
                 <div class="team-info"><p>Marketing Manager</p></div>
-                <p>Elisa first fell in love with digital marketing at the university. He loves to learn, and looks forward to being part of this new exciting industry for many years.</p>
+                <p>Kumar first fell in love with digital marketing at the university. He loves to learn, and looks forward to being part of this new exciting industry for many years.</p>
               </div>
             </div>
           </div>
@@ -54,18 +56,18 @@ const Hero = (props) => {
       </section>
       <section>
         <h2>Our Work</h2>
-        <p>771 million people lack basic access to clean and safe drinking water. We've been on a mission to end the water crisis since 2006, and with the help of generous supporters like you, we're getting closer every day.</p>
+        <p>771 million people lack basic access to clean and safe drinking water. We've been on a mission to end the water crisis since 2014, and with the help of generous supporters like you, we're getting closer every day.</p>
         <ul>
           <li>
-            <h3>120,78</h3>
+            <h3>2,784</h3>
             <p>Water projects funded</p>
           </li>
           <li>
-            <h3>16,835,62</h3>
+            <h3>835,671</h3>
             <p>People will be served</p>
           </li>
           <li>
-            <h3>20</h3>
+            <h3>15</h3>
             <p>States in India</p>
           </li>
         </ul>
@@ -87,7 +89,7 @@ const Hero = (props) => {
       <section>
         <h2>Proving Our Impact</h2>
         <p>From the start, we’ve publicly shared photos and GPS coordinates for every single charity: water project. You can find all of our funded projects on the map above.</p>
-        <button>VIEW MAP</button>
+        <a href="https://www.google.com/maps/d/u/0/embed?mid=1RzMmcAX2n2QDiXzAcKmX4tdWdlSHtCY&ehbc=2E312F" target="_blank"><button>VIEW MAP</button></a>
       </section>
 
       <section>
@@ -103,18 +105,18 @@ const Hero = (props) => {
       <section>
         <h2>Add Impact To Your Inbox</h2>
         <p>Get our emails to stay in the know.</p>
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
           <label htmlFor="fname">First name:</label><br />
           <input type="text" id="fname" name="fname" /><br />
           <label htmlFor="lname">Last name:</label><br />
           <input type="text" id="lname" name="lname" /><br /><br />
           <label htmlFor="email">Email:</label><br />
           <input type="email" id="email" name="email" /><br /><br />
-          <input type="submit" value="SUBSCRIBE" />
+          <input type="submit" value="SUBSCRIBE" onClick={() => navigate('/register')} />
         </form>
       </section>
 
-    </>
+    </div>
   );
 }
 
